3e2c73f1f5034c4e3b034378c5eef1fe91da041a
tejasv01 <tejasvlata870@gmail.com>
2026-04-26 01:01:17 +0530

    added basic backend

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+import connectDB from "./config/database.js";
+import dotenv from 'dotenv';
+import app from './app.js'
+
+dotenv.config({
+    path: './.env'
+});
+
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server running on port: ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log(`MongoDB connection failed.`, error);
+    }
+}
+
+startServer();
