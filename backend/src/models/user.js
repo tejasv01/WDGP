0d00c4892d1b3d5702610aa9a4c618c06c2e52ef
tejasv01 <tejasvlata870@gmail.com>
2026-04-27 00:22:17 +0530

    added user model

---
+++ backend/src/models/user.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    profileImage: {
+        type: String,
+        default: "https://default-avatar-url.com/avatar.png"
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    likedSongs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Song'
+    }],
+    playlists: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Playlist'
+    }]
+}, {
+    timestamps: true
+});
+
+export const User = mongoose.model('User', userSchema);
